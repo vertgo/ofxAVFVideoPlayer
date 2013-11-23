91658dfbd1f8bbca1f7807ad11c2d71d1f1c52b3
Elie Zananiri <prisonerjohn@gmail.com>
2013-11-23 12:02:07 -0500

    Stripped out all the extra stuff from the basic example.

--- example/src/testApp.cpp
+++ example/src/testApp.cpp
@@ -1,115 +1,30 @@
 #include "testApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
-    for(int i=0; i<N_VIDEO_PLAYERS; i++) {
-        videoPlayers.push_back(new ofxAVFVideoPlayer());
-        videoPlayers[i]->loadMovie("/Users/focus/Desktop/RGBD_Compiled/Casey_tangle.mov");
-    }
-    
-    
+void testApp::setup()
+{
     ofSetVerticalSync(true);
+    ofSetRectMode(OF_RECTMODE_CENTER);
     
-    
-	FFTanalyzer.setup(44100, BUFFER_SIZE/2, 2);
-	
-	FFTanalyzer.peakHoldTime = 15; // hold longer
-	FFTanalyzer.peakDecayRate = 0.95f; // decay slower
-	FFTanalyzer.linearEQIntercept = 0.9f; // reduced gain at lowest frequency
-	FFTanalyzer.linearEQSlope = 0.01f; // increasing gain at higher frequencies
-
-    
+    video.loadMovie("test.mov");
+    video.play();
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
-    int i=0;
-    for(auto p : videoPlayers) {
-        p->update();
-        if(true || p->isLoaded()) {
-            if(ofGetElapsedTimef() > i++ * 0.5)
-                p->play();
-        }
-    }
-        
-    //cout << ofGetFrameRate() << endl;
+void testApp::update()
+{
+    video.update();
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
-    int i=0;
-    for(auto p : videoPlayers) {
-        // draw video
-        ofSetColor(ofColor::white);
-        ofFill();
-		p->draw(0,0);
-        
-        // draw audio waveform
-        ofSetColor(ofColor::red);
-        ofNoFill();
-        ofBeginShape();
-        for (int i = 0; i < ofGetWidth(); i++) {
-            ofVertex(i, ofGetHeight() / 2 + p->getAmplitudeAt(i / (float)ofGetWidth()) * ofGetHeight() / 2);
-        }
-        ofEndShape();
-        ofRect(0, ofGetHeight() - 20, p->getAmplitude() * ofGetWidth(), 20);
-        
-        // draw playhead over the waveform
-        ofSetColor(ofColor::white);
-        ofLine(p->getPosition() * ofGetWidth(), 0, p->getPosition() * ofGetWidth(), ofGetHeight());
-        
-        // draw current amplitude at the bottom
-        ofFill();
-        ofRect(0, ofGetHeight() - 20, ofGetWidth() * ABS(p->getAmplitude()), 20);
-        
-        // calculate fft
-        float avg_power = 0.0f;
-        
-        int idx = MIN(floor(p->getPosition() * p->getNumAmplitudes()), p->getNumAmplitudes() - 1);
-        myfft.powerSpectrum(idx, (int)BUFFER_SIZE/2, p->getAllAmplitudes(), BUFFER_SIZE, &magnitude[0], &phase[0], &power[0], &avg_power);
-        
-        for (int i = 0; i < (int)(BUFFER_SIZE/2); i++) {
-            freq[i] = magnitude[i];
-        }
-        
-        FFTanalyzer.calculate(freq);
-        
-        // draw fft bands
-        ofSetHexColor(0xffffff);
-        // This draws frequency bands (lots of em!)
-//        for (int i = 0; i < (int)(BUFFER_SIZE/2 - 1); i++) {
-//            ofRect(20 + (i*4), ofGetHeight() - 40, 4, -freq[i] * 10.0f);
-//        }
-        
-        for (int i = 0; i < FFTanalyzer.nAverages; i++) {
-            ofRect(ofGetWidth() / 2 + (i*20), ofGetHeight() - 40, 20, -FFTanalyzer.averages[i] * 6);
-        }
-        
-        ofSetHexColor(0xff0000);
-        for (int i = 0; i < FFTanalyzer.nAverages; i++) {
-            ofRect(ofGetWidth() / 2 + (i*20), ofGetHeight() - 40 - FFTanalyzer.peaks[i] * 6 - 100, 20, -4);
-        }
-    }
-
+void testApp::draw()
+{
+    video.draw(ofGetMouseX(), ofGetMouseY());
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-    switch(key) {
-        case '1':
-            videoPlayers[0]->loadMovie("IntroVideo7.mov");
-            break;
-        case '2':
-            videoPlayers[1]->loadMovie("TheLumineers_1.mov");
-            break;
-        case '3':
-            videoPlayers[2]->loadMovie("EmeliSande_NextToMe.mov");
-            break;
-        case '4':
-            videoPlayers[3]->loadMovie("iHRMF2012_SwedishHouseMafia_DontWorryChild.mov");
-            break;
-    }
-//    videoPlayer2.loadMovie("IntroVideo7.mov");
+
 }
 
 //--------------------------------------------------------------
@@ -118,7 +33,7 @@ void testApp::keyReleased(int key){
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
+void testApp::mouseMoved(int x, int y){
 
 }
 
--- example/src/testApp.h
+++ example/src/testApp.h
@@ -3,22 +3,16 @@
 #include "ofMain.h"
 #include "ofxAVFVideoPlayer.h"
 
-#include "fft.h"
-#include "FFTOctaveAnalyzer.h"
-
-#define BUFFER_SIZE 512
-
-
-class testApp : public ofBaseApp{
-
+class testApp : public ofBaseApp
+{
 	public:
 		void setup();
 		void update();
 		void draw();
 
-		void keyPressed  (int key);
+		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y );
+		void mouseMoved(int x, int y);
 		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
@@ -26,14 +20,5 @@ class testApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
     
-        std::vector<ofxAVFVideoPlayer *> videoPlayers;
-        static const int N_VIDEO_PLAYERS = 1;
-    
-    float magnitude[BUFFER_SIZE];
-    float phase[BUFFER_SIZE];
-    float power[BUFFER_SIZE];
-    float freq[BUFFER_SIZE/2];
-    
-    FFTOctaveAnalyzer FFTanalyzer;
-    fft		myfft;
+        ofxAVFVideoPlayer video;
 };
